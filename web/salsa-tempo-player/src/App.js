d99dd084e7dd83ff4663b0bc8974503f5bd9a88d
jesus <jesusugarte10@gail.com>
2024-11-20 19:02:08 -0500

    fixing index in public

--- web/salsa-tempo-player/src/App.js
+++ web/salsa-tempo-player/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import Player from './components/Player';
 import SpotifyAuth from './components/SpotifyAuth';
 
